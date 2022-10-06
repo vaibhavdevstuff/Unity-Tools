89dcce75fefed915b5a0ce90a635d09bc48c0d32
vaibhavdevstuff <vaibhav-jaiswal@outlook.com>
2022-10-06 13:28:10 +0530

    Update MissingComponetsAndPrefabs.cs

--- Editor Tools/Missing Componets And Prefabs/Editor/MissingComponetsAndPrefabs.cs
+++ Editor Tools/Missing Componets And Prefabs/Editor/MissingComponetsAndPrefabs.cs
@@ -6,20 +6,27 @@ using UnityEditor;
 
 namespace DominoCode
 {
-
     public class MissingComponetsAndPrefabs
     {
         [MenuItem("Tools/Domino Code/Log Missing Components and Prefabs")]
         public static void LogMissingComponentsAndPrefabs()
         {
+            //Clear the previous missing data
             missingDataResult.Clear();
 
+            //Start From getting all the root objects in Scene
             GameObject[] rootGO = SceneManager.GetActiveScene().GetRootGameObjects();
 
-            foreach (GameObject go in rootGO) FindMissingOne(go);
+            //Find Missing Refence in Root Objects and their Childrens
+            foreach (GameObject go in rootGO)
+            {
+                FindMissingOne(go);
+            }
 
+            //Total Missing Refence Found
             Debug.Log("Total Missing: " + missingDataResult.Count);
 
+            //Log All Missing Prefabs & Components 
             for (int i = 0; i < missingDataResult.Count; i++)
             {
                 Debug.Log(missingDataResult[i].path, missingDataResult[i].missingGO);
@@ -27,19 +34,30 @@ namespace DominoCode
 
         }
 
+        //Stores Missing Data
         public static List<MissingData> missingDataResult = new List<MissingData>();
 
+        /// <summary>
+        /// Find Missing Reference
+        /// </summary>
+        /// <param name="go">GameObject to check Missing Refence</param>
+        /// <param name="path">path of this GameObject</param>
         public static void FindMissingOne(GameObject go, string path = "")
         {
+            //Set Default path for Root Object and Path derive from parent for children object
             string currentPath = path + " / " + go.name;
+            
             Component[] components = go.GetComponents<Component>();
 
+            //Take gameObject as Prefab
             PrefabAssetType pt = PrefabUtility.GetPrefabAssetType(go);
 
+            //Check if prefab is Missing
             if (pt == PrefabAssetType.MissingAsset)
             {
                 StorePrefabData(currentPath + " / " + go.name, go);
             }
+            //Check for its Components
             else
             {
                 for (int i = 0; i < components.Length; i++)
@@ -49,6 +67,7 @@ namespace DominoCode
                 }
             }
 
+            //Check For missing reference in Child
             for (int j = 0; j < go.transform.childCount; j++)
             {
                 GameObject childObject = go.transform.GetChild(j).gameObject;
@@ -60,6 +79,7 @@ namespace DominoCode
                 }
                 else
                 {
+                    //Again Check for all missing refence in child taking current child as main Object
                     FindMissingOne(childObject, currentPath);
                 }
             }
@@ -67,6 +87,11 @@ namespace DominoCode
 
         }
 
+        /// <summary>
+        /// Store Missing Component Data
+        /// </summary>
+        /// <param name="path">Component's GameObject Path</param>
+        /// <param name="go">Component's GameObject</param>
         public static void StoreComponentData(string path, GameObject go)
         {
             MissingData result;
@@ -75,6 +100,12 @@ namespace DominoCode
 
             missingDataResult.Add(result);
         }
+
+        /// <summary>
+        /// Store Missing Prefab Data
+        /// </summary>
+        /// <param name="path">Prefab Gameobject Path</param>
+        /// <param name="go">Prefab GameObject</param>
         public static void StorePrefabData(string path, GameObject go)
         {
             MissingData result;
